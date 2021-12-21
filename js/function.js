0c97fc4754b65c92bdfd5336a5ad76cfd41e3783
Michele <Michele.vir@hotmail.com>
2021-12-21 14:11:27 +0100

    Cambiato lo stile dell'inventario, aggiunto function.js, cambiata funzione di login

---
+++ js/function.js
@@ -0,0 +1,24 @@
+function logOut(){
+    sessionStorage.removeItem('authToken')
+    localStorage.removeItem('authToken')
+
+    document.location.href="../../nauthHome.html"
+}
+
+function serializeFormJson(form) {
+    return Array.from(form.querySelectorAll('input, select, textarea'))
+        .filter(element => element.name)
+        .reduce((json, element) => { json[element.name] = element.type === 'checkbox' ? element.checked : element.value; return json; }, {});
+}
+
+async function logIn(loginForm){
+    const data = serializeFormJson(loginForm);
+
+    const response = await api.employees.login(data);
+
+    if (response.status == 200)
+        config.setToken(response.headers.authorization, data.remember)
+    console.log(config.getToken())
+    document.location.href = "/html/authenticatedPage/authIndex.html"
+
+}
