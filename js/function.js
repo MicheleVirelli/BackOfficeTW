115e5fd35d4670bf41d4232b2f633de29706e3cb
Michele <Michele.vir@hotmail.com>
2021-12-26 20:16:04 +0100

    Gestita la paginazione nauth

--- js/function.js
+++ js/function.js
@@ -45,13 +45,62 @@ function paginator(elemForPage, array) {
   return paginate;
 }
 
-function refreshNavPage(array) {
+// TODO: aggiungere il nome della variabile passata
+function refreshNavPage(array, arrayName) {
   $("#navlist").children().remove();
   pageNum = 0;
   array.forEach((page) => {
     $("#navlist").append(`
-    <li class="page-item"><a class="page-link" href="#" onclick="refreshTable(${pageNum},paginate)">${pageNum++}</a></li>
+    <li class="page-item"><a class="page-link" href="#" onclick="refreshNauthTable(${arrayName},${pageNum})">${pageNum+1}</a></li>
     `);
     pageNum++;
   });
+}
+
+function productPage(id) {
+  localStorage.removeItem('idProduct')
+  localStorage.setItem("idProduct", id);
+  document.location.href = "nauthProductPage.html";
+}
+
+function refreshNauthTable(paginateArray, value) {
+  let boolColor = true
+
+  $("#productlist").children().remove();
+
+  $("#productlist").append(`
+  <table id="nauthTable" class="table">
+    <tr  class="${boolColor ? 'td' : 'tr'}">
+      <td>Nome Prodotto</td>
+      <td>Categoria</td>
+      <td>Sottocategoria</td>
+    </tr>
+  </table>
+  `);
+
+  console.log("Refresh with " + value);
+  console.log(paginateArray);
+
+  paginateArray[value].forEach((product) => {
+    boolColor = !boolColor
+    $("#nauthTable").append(`
+      <tr class="clickable-row ${boolColor ? 'td' : 'tr'}" onclick="productPage('${product._id}')">
+        <td>${product.name}</td>
+        <td>${product.category}</td>
+        <td>${product.subcategory}</td>
+      </tr>
+    `);
+  });
+}
+
+function filterBy(array, field, value) {
+  let filtered = []
+
+  array.forEach(element => {
+    if(element != undefined)
+      if(element[field].includes(value))
+        filtered.push(element)
+  });
+
+  return filtered
 }
